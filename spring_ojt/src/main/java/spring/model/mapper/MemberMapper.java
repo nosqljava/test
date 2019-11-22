708c0575b65c1ed009e1459364222b9238cbe295
soldesk <soldesk@soldesk-PC>
2019-11-22 12:05:07 +0900

    Spring_ojt 프로젝트 생성

---
+++ spring_ojt/src/main/java/spring/model/mapper/MemberMapper.java
@@ -0,0 +1,9 @@
+package spring.model.mapper;
+
+
+
+public interface MemberMapper {
+
+
+	
+}
---
+++ spring_ojt/src/main/webapp/js/breply.js
@@ -0,0 +1,128 @@
+console.log("*****Reply Module........");
+var replyService = (function() {
+	function getList(param, callback, error) {
+		var bbsno = param.bbsno;
+		var sno = param.sno;
+		var eno = param.eno;
+		// alert(param.bbsno);
+		$.getJSON("./reply/list/" + bbsno + "/" + sno + "/" + eno + ".json",
+				function(data) {
+					// alert(data);
+					if (callback) {
+						callback(data); // 댓글 목록만 가져오는 경우
+						// callback(data.replyCnt, data.list); //댓글 숫자와 목록을 가져오는
+						// 경우
+					}
+				});
+
+	}
+
+	function getPage(param, callback, error) {
+
+		$.ajax({
+			type : 'get',
+			url : "./reply/page",
+			data : param,
+			contentType : "application/text; charset=utf-8",
+			success : function(result, status, xhr) {
+				if (callback) {
+					callback(result);
+				}
+			},
+			error : function(xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+
+	function add(reply, callback, error) {
+		console.log("add reply...............");
+
+		$.ajax({
+			type : 'post',
+			url : './reply/create',
+			data : JSON.stringify(reply),
+			contentType : "application/json; charset=utf-8",
+			success : function(result, status, xhr) {
+				if (callback) {
+					callback(result);
+				}
+			},
+			error : function(xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+
+	function get(rnum, callback, error) {
+
+		$.get("./reply/" + rnum + ".json", function(result) {
+
+			if (callback) {
+				callback(result);
+			}
+
+		}).fail(function(xhr, status, err) {
+			if (error) {
+				error();
+			}
+		});
+	}
+
+	function update(reply, callback, error) {
+
+		console.log("rnum: " + reply.rnum);
+
+		$.ajax({
+			type : 'put',
+			url : './reply/' + reply.rnum,
+			data : JSON.stringify(reply),
+			contentType : "application/json; charset=utf-8",
+			success : function(result, status, xhr) {
+				if (callback) {
+					callback(result);
+				}
+			},
+			error : function(xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+
+	function remove(param, callback, error) {
+		$.ajax({
+			type : 'delete',
+			url : './reply/'+param.rnum+'/'+param.bbsno+'/'+param.nPage+'.json',
+			
+			success : function(deleteResult, status, xhr) {
+				if (callback) {
+					callback(deleteResult);
+				}
+			},
+			error : function(xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+	
+	
+	;
+	return {
+		getList : getList,
+		getPage : getPage,
+		add : add,
+		get : get,
+		update : update,
+		remove : remove
+		
+	}
+
+})();
